055d648e50ce0a1f47457aa77f8017d895d89d4c
Abhimanu <abhimanu.sharma@softobiz.com>
2020-12-02 12:06:11 +0530

    Fixed addClient api

--- views/src/components/AddClient.js
+++ views/src/components/AddClient.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Alert } from 'reactstrap';
 
 
 class AddClient extends Component {
 
   state = {
-    client_id: null,
+    client_id: "",
     client_name: "",
     company_name: "",
     position: "",
-    tel: null,
+    tel: "",
     email: "",
     last_contacted_on: "",
+    visible: false,
+    color: 'success',
+    msg: '',
   }
 
-  addClient = () => {
+  formatDate = (date) => {
+    var d = new Date(date);
+    return (d.getMonth + 1) + '-' + d.getDate() + '-' + d.getFullYear();
+  }
+
+  showAlert = (color, msg) => {
+    console.log("showAlert!!!");
+    this.setState({color:color});
+    this.setState({msg:msg});
+    this.setState({visible:true},()=>{
+      window.setTimeout(()=>{
+        this.setState({visible:false})
+      },2000)
+    });
+  }
+
+  addClient = (e) => {
+  
     const client = {
       client_id: this.state.client_id,
       client_name: this.state.client_name,
@@ -25,17 +46,20 @@ class AddClient extends Component {
       last_contacted_on: this.state.last_contacted_on,
     }
 
-    if(Object.keys(client).length === 0 && client.constructor === Object){
+    if(Object.keys(client).length !== 0 && client.constructor === Object){
       axios.post('/api/clients', client)
         .then(res => {
           if(res.data){
+
+            this.showAlert("success", "Clients added!");
+
             this.props.getClients();
             this.setState({
-              client_id: null,
+              client_id: "",
               client_name: "",
               company_name: "",
               position: "",
-              tel: null,
+              tel: "",
               email: "",
               last_contacted_on: ""
             })
@@ -49,13 +73,7 @@ class AddClient extends Component {
 
   handleChange = (e) => {
     this.setState({
-      client_id: null,
-      client_name: "",
-      company_name: "",
-      position: "",
-      tel: null,
-      email: "",
-      last_contacted_on: ""
+      [e.target.name] : e.target.value
     })
   }
 
@@ -67,31 +85,39 @@ class AddClient extends Component {
       position,
       tel,
       email,
-      last_contacted_on 
+      last_contacted_on,
+      color,
+      visible,
+      msg
     } = this.state;
 
     return (
       <div>
-        <input type="hidden" value={client_id} />
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={client_name} />
-        </div>
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={company_name} />
-        </div>
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={position} />
-        </div>
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={tel} />
-        </div>
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={email} />
-        </div>
-        <div className="form-group">
-          <input type="text" class="form-control" onChange={this.handleChange} value={last_contacted_on} />
-        </div>
-        <button className="btn btn-success" onClick={this.addClient}>add Client</button>
+        <form>
+          <div className="form-group">
+            <input type="text" name="client_id" placeholder="Client ID" className="form-control" onChange={this.handleChange} value={client_id} required />
+          </div>
+          <div className="form-group">
+            <input type="text" name="client_name" placeholder="Client name" className="form-control" onChange={this.handleChange} value={client_name} required />
+          </div>
+          <div className="form-group">
+            <input type="text" name="company_name" placeholder="Company name" className="form-control" onChange={this.handleChange} value={company_name} required />
+          </div>
+          <div className="form-group">
+            <input type="text" name="position" placeholder="Position" className="form-control" onChange={this.handleChange} value={position} required />
+          </div>
+          <div className="form-group">
+            <input type="text" name="tel" placeholder="Phone no." className="form-control" onChange={this.handleChange} value={tel} required />
+          </div>
+          <div className="form-group">
+            <input type="email" name="email" placeholder="Email ID" className="form-control" onChange={this.handleChange} value={email} required />
+          </div>
+          <div className="form-group">
+            <input type="date" name="last_contacted_on" placeholder="Last contacted on" className="form-control" onChange={this.handleChange} value={last_contacted_on} required />
+          </div>
+          <Alert color={color} isOpen={visible}>{msg}</Alert>
+          <button type="button" className="btn btn-success" onClick={this.addClient}>add Client</button>
+        </form>
       </div>
     )
   }
